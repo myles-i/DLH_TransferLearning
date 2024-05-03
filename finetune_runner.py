077e8cc107489b0b29697ab434cf66e1e2923834
Myles <myles.iribarne@gmail.com>
2024-05-03 14:03:13 -0300

    add 1, and 100 choices to weights-type

--- finetune_runner.py
+++ finetune_runner.py
@@ -24,7 +24,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--weights-type",
-        choices=("random", "20", "10"),
+        choices=("random", "20", "10", "1", "100"),
         required=True,
         help="Weight type",
     )
