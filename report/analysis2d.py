2a3219521b43666337952cfc160918aae1418c18
Daniel Xu <dhhxu88@gmail.com>
2024-05-04 22:45:16 -0700

    Complete move of analysis code to py files

---
+++ report/analysis2d.py
@@ -0,0 +1,132 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import common
+
+
+def make_epoch_table(history_all, decimals=1):
+    table = (
+        history_all.groupby(["weight_type", "seed"])
+        .max()
+        .groupby("weight_type")
+        .agg(
+            Mean=pd.NamedAgg("epoch", "mean"),
+            Std=pd.NamedAgg("epoch", "std"),
+        )
+        .round(decimals)
+        .reindex(["random", "10", "20"])
+        .rename(
+            index={
+                "random": "Random",
+                "10": "Pre-train 10",
+                "20": "Pre-train 20",
+            }
+        )
+        .reset_index()
+        .rename(
+            columns={
+                "weight_type": "Scenario",
+            }
+        )
+    )
+    # Explicitly select columns in desired order.
+    table = table[["Scenario", "Mean", "Std"]]
+    return table
+
+
+def plot_f1_by_epoch(history_all):
+    data = common._prepare_f1_for_plot(history_all)
+    fig, ax = plt.subplots()
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Average Macro F1")
+    ax.plot(
+        "epoch",
+        "mean",
+        color="tab:red",
+        data=data.loc["random"],
+        label="Random",
+    )
+    ax.plot(
+        "epoch",
+        "mean",
+        color="tab:blue",
+        data=data.loc["10"],
+        label="Pre-train 10",
+    )
+    ax.plot(
+        "epoch",
+        "mean",
+        color="tab:orange",
+        data=data.loc["20"],
+        label="Pre-train 20",
+    )
+    ax.legend()
+    # Replicate y-axis in Figure 3(a) of the paper.
+    ax.set_ylim([0.5, 1.0])
+
+
+def plot_f1_by_epoch_with_range(history_all):
+    # Comparable to Figure 3(a) in the paper.
+    data = common._prepare_f1_for_plot(history_all)
+    fig, ax = plt.subplots()
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Average Macro F1")
+    ax.plot(
+        "epoch",
+        "mean",
+        color="tab:red",
+        data=data.loc["random"],
+        label="Random",
+    )
+    ax.plot("epoch", "mean", data=data.loc["10"], label="Pre-train 10")
+    ax.plot("epoch", "mean", data=data.loc["20"], label="Pre-train 20")
+    ax.legend()
+
+    # Add min, max range
+    ax.fill_between(
+        "epoch",
+        "min",
+        "max",
+        data=data.loc["random"],
+        color="tab:red",
+        alpha=0.15,
+    )
+    ax.fill_between("epoch", "min", "max", data=data.loc["10"], alpha=0.15)
+    ax.fill_between("epoch", "min", "max", data=data.loc["20"], alpha=0.15)
+    # Replicate y-axis in Figure 3(a) of the paper.
+    ax.set_ylim([0.5, 1.0])
+
+
+def make_f1_table(predictions):
+    """
+    Actual function to build a combined table with average macro F1 and
+    average per-class F1 scores for each scenario.
+    """
+    macro_f1 = common._make_macro_f1_table(predictions)
+    per_class_f1 = common._make_per_class_f1_table(predictions)
+    table = (
+        macro_f1.join(per_class_f1)
+        .reindex(["random", "10", "20"])
+        .rename(
+            index={
+                "random": "Random",
+                "10": "Pre-train 10",
+                "20": "Pre-train 20",
+            }
+        )
+        .reset_index()
+        # Using same names as Table 1 in the paper.
+        .rename(
+            columns={
+                "weight_type": "Type",
+                "macro_f1": "F1",
+                "N_f1": "F1n",  # Normal
+                "A_f1": "F1a",  # AF
+                "O_f1": "F1o",  # Other
+                "~_f1": "F1p",  # Noisy
+            }
+        )
+    )
+    # Explicitly select columns in desired order.
+    table = table[["Type", "F1", "F1n", "F1a", "F1o", "F1p"]]
+    return table
---
+++ report/common.py
@@ -0,0 +1,119 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import f1_score
+
+
+def my_f1(y_true, y_prob, average="macro"):
+    # set average=None to get per-class F1 scores.
+    y_pred = y_prob >= np.max(y_prob, axis=1)[:, None]
+    return f1_score(y_true, y_pred, average=average)
+
+
+def _prepare_f1_for_plot(history_all):
+    """
+    For use by functions that plot F1 by epoch.
+    """
+    data = (
+        history_all.groupby(["weight_type", "epoch"])
+        .agg(
+            min=pd.NamedAgg("f1", "min"),
+            max=pd.NamedAgg("f1", "max"),
+            mean=pd.NamedAgg("f1", "mean"),
+        )
+        # Move epoch from index to column
+        .reset_index("epoch")
+    )
+    return data
+
+
+def _make_macro_f1_table(predictions):
+    """
+    Build table with average macro F1 scores for each scenario.
+
+    For use by make_f1_table().
+    """
+    macro = []
+    for key, value in predictions.items():
+        weight_type, seed = key.split("_")
+        identifier = {"weight_type": weight_type, "seed": seed}
+        # Details about my_f1 are in the Evaluation > Fine-tuning section.
+        macro_f1 = my_f1(value["y_true"], value["y_prob"], average="macro")
+        macro_f1 = {"f1": macro_f1}
+        macro.append(identifier | macro_f1)
+
+    macro_scores = pd.DataFrame(macro)
+    macro_df = macro_scores.groupby(["weight_type"])[["f1"]].agg(
+        mean=pd.NamedAgg("f1", "mean"),
+        std=pd.NamedAgg("f1", "std"),
+    )
+    macro_df["macro_f1"] = _make_mean_std_column(macro_df["mean"], macro_df["std"])
+    macro_df = macro_df[["macro_f1"]]
+    return macro_df
+
+
+def _make_per_class_f1_table(predictions):
+    """
+    Build table with average per-class F1 scores for each scenario.
+
+    For use by make_f1_table().
+    """
+    per_class = []
+    for key, value in predictions.items():
+        weight_type, seed = key.split("_")
+        identifier = {"weight_type": weight_type, "seed": seed}
+
+        class_f1 = my_f1(value["y_true"], value["y_prob"], average=None)
+        class_f1 = dict(zip(value["classes"], class_f1))
+        per_class.append(identifier | class_f1)
+
+    per_class_scores = pd.DataFrame(per_class)
+    per_class_macro = per_class_scores.groupby(["weight_type"])[
+        ["N", "A", "O", "~"]
+    ].agg(
+        **{
+            "N_mean": pd.NamedAgg("N", "mean"),
+            "A_mean": pd.NamedAgg("A", "mean"),
+            "O_mean": pd.NamedAgg("O", "mean"),
+            "~_mean": pd.NamedAgg("~", "mean"),
+            "N_std": pd.NamedAgg("N", "std"),
+            "A_std": pd.NamedAgg("A", "std"),
+            "O_std": pd.NamedAgg("O", "std"),
+            "~_std": pd.NamedAgg("~", "std"),
+        }
+    )
+
+    per_class_macro["N_f1"] = _make_mean_std_column(
+        per_class_macro["N_mean"], per_class_macro["N_std"]
+    )
+    per_class_macro["A_f1"] = _make_mean_std_column(
+        per_class_macro["A_mean"], per_class_macro["A_std"]
+    )
+    per_class_macro["O_f1"] = _make_mean_std_column(
+        per_class_macro["O_mean"], per_class_macro["O_std"]
+    )
+    per_class_macro["~_f1"] = _make_mean_std_column(
+        per_class_macro["~_mean"],
+        per_class_macro["~_std"],
+    )
+    per_class_macro = per_class_macro[["N_f1", "A_f1", "O_f1", "~_f1"]]
+    return per_class_macro
+
+
+def _make_mean_std_column(mean_col, std_col, decimals=3):
+    """
+    Helper to produce column values formatted like in Table 1 of the paper.
+    """
+    return (
+        mean_col.astype(str)
+        # 1: decimal point's index, e.g.: '0.xxx'
+        # 1 + decimals + 1: (decimals + 1) is total string length, counting
+        # the decimal point itself. The (1 +) is because we started the
+        # substring at index 1.
+        .str[1 : 1 + decimals + 1]
+        .str.pad(decimals + 1, side="right", fillchar="0")
+        + " (\u00b1 "  # plus minus symbol
+        + std_col.astype(str)
+        .str[1 : 1 + decimals + 1]
+        .str.pad(decimals + 1, side="right", fillchar="0")
+        + ")"
+    )
